811a77682b343f3048f3fd1ea81ee3e0d6c4671f
Natasha01 <borodycknatasha01@mail.ru>
2018-02-21 20:57:47 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -4,4 +4,31 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var count = 0;
+    var is_involved = [];
+    var a;
+    var b;
+    var c;
+    for (var i = 0; i < preferences.length; i++){
+        is_involved[i] = false;
+    }
+    //var current_spichonee = 0;
+    for (var current_spichonee = 0; current_spichonee < preferences.length; current_spichonee++){
+        if (!(is_involved[current_spichonee]) && (preferences[current_spichonee]) != (current_spichonee + 1)) {
+            a = current_spichonee + 1;
+            b = preferences[current_spichonee];
+            c = preferences[b - 1];
+            if (preferences[c - 1] === a){
+                count++;
+                is_involved[a - 1] = true;
+                is_involved[b - 1] = true;
+                is_involved[c - 1] = true;
+                a = 0;
+                b = 0;
+                c = 0;
+            }           
+        }
+        
+    }
+    return count;
 };
